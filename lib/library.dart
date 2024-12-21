c2312c167a3e49c82bd9380189fd7e2976f0c8ad
Hanan-Ashraf-Mousa <mosah2312@gmail.com>
2024-12-21 17:41:12 +0200

    modify borrow & return  method.

--- lib/library.dart
+++ lib/library.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'user.dart';
 import 'book.dart';
 
@@ -14,11 +16,12 @@ class Library{
    users.add(user);
   }
   returnBook(String bookId){
-   // check if book is already existed in list
+   // check if book with bookId is existed in list and return it
   Book? book = books.firstWhere(
-          (element)=>  element.bookId == bookId
+          (element)=>  element.bookId == bookId,
+      orElse: ()=>Book(bookId: '',bookTitle: '')
   );
-  if(book != null && book.isBorrow){
+  if(book.bookId.isNotEmpty && book.isBorrow){
     book.isBorrow = false;
     print('Book with ID $bookId returned');
   }else{
@@ -26,17 +29,22 @@ class Library{
   }
   }
 
-  borrowBook(String bookId , String userId){
-    // check if book is already existed in list
-   Book? book = books.firstWhere((element) => element.bookId == bookId);
-   User? user = users.firstWhere((element) => element.id == userId);
+  borrowBook(String bookId , int userId){
+    // check if book with bookId is existed in list and return it
+   Book book = books.firstWhere(
+           (element) => element.bookId == bookId,
+       orElse: ()=>Book(bookId: '',bookTitle: '') );
 
-   if(book != null && book.isBorrow == false){
-     if(user !=null){
+   User user = users.firstWhere((element) => element.id == userId, orElse: ()=>User(-1,''));
+
+   if(book.bookId.isNotEmpty && book.isBorrow == false){
+     if(user.id != -1){
      book.isBorrow = true ;
      print('Book with ID $bookId borrowed');
    }else{
-      users.add(user);
+       print('Please , enter your name');
+      String name = stdin.readLineSync()!;
+      users.add(User(userId,name));
       book.isBorrow = true ;
       print('Book with ID $bookId borrowed');
   }
