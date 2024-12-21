cd8f553b2f3dff1e1802488780e6c08a7e40fc27
Hanan-Ashraf-Mousa <mosah2312@gmail.com>
2024-12-21 18:54:02 +0200

    upload library system

--- lib/library_system.dart
+++ lib/library_system.dart
@@ -1,3 +1,28 @@
-int calculate() {
-  return 6 * 7;
+import 'package:library_system/book.dart';
+import 'package:library_system/library.dart';
+import 'package:library_system/user.dart';
+
+void main() {
+Book book1 = Book(bookId:'1' , bookTitle:'The Old Man and The sea' );
+Book book2 = Book(bookId: '2', bookTitle: "Harry Potter and the Sorcerer's Stone" );
+
+User user1 = User(1, 'Omar');
+User user2 = User(2,'Ali');
+
+Library library = Library();
+
+library.addBook(book1);
+library.addBook(book2);
+library.addUser(user1);
+library.addUser(user2);
+
+library.borrowBook('1', 1);
+library.displayLibraryInfo();
+print('------------------------');
+library.borrowBook('2', 3);
+library.displayLibraryInfo();
+print('------------------------');
+library.returnBook('1');
+library.displayLibraryInfo();
+
 }
